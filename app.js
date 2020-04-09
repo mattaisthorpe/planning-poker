0d83362818a9f636f3bb91bc62a32a1e759cf5b2
Matthew Aisthorpe <m.aisthorpe@hotmail.com>
2020-04-09 14:24:42 +1000

    Added human readable names and room names, can update task name, room now list users and highlights host

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 const app = express();
 const http = require('http').createServer(app);
 const port = 3000;
 const io = require('socket.io')(http);
+const randomNames = require('./config/randomnames');
 
 //global settings
 app.locals.title = 'Planning Poker';
 const deck = [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100];
 const defaultRoom = {
-    users: 0,
+    name: '',
+    task: '',
+    users: {},
     played: 0,
     numbers: [],
-    host: ''
+    host: '',
+    chatHistory: {}
 }
 let pokerRooms = {};
-
-//Loads the handlebars module and helpers
-const handlebars = require('express-handlebars');
 
 //Use the handlebars engine
 app.set('view engine', 'hbs');
@@ -47,26 +49,37 @@ app.get('/room', (req, res) => {
 io.on('connection', function (socket) {
 
     //connect to a room
-    socket.on('connectToRoom', function (room) {
+    socket.on('connectToRoom', function (roomData) {
+        const room = roomData.hash;
         if (socket.room) {
             socket.leave(socket.room);
         }
         socket.join(room, function () {
-            let isHost = false;
+            socket.isHost = false;
             socket.room = room;
+            socket.username = userName(roomData.username);
+
+            //create a default room if one isn't made
             if (!pokerRooms.hasOwnProperty(room)) {
+                console.log('Creating new room' + room);
                 pokerRooms[room] = defaultRoom;
                 pokerRooms[room].host = socket.id;
-                isHost = true;
+                pokerRooms[room].name = roomName(roomData.roomname);
+                socket.isHost = true;
                 roomList(false);
             }
-            io.in(room).clients((err, clients) => {
-                //console.log(clients);
-                pokerRooms[room].users = clients.length;
-                //console.log(pokerRooms);
-            });
-            socket.emit('connectedToRoom', { is_host: isHost, message: 'Welcome ' + socket.id + ', You have connected to room ' + room });
-            socket.broadcast.emit('chatMessage', 'Welcome ' + socket.id + ' to the room');
+
+            //update amount of users in room
+            pokerRooms[room].users[socket.id] = {
+                name: socket.username, 
+                is_host: socket.isHost,
+                played: false
+            };
+
+            //announce user has joined room
+            updateUsers(room);
+            chatMessage(room, 'Welcome ' + socket.username + ' to the room', true);
+            socket.emit('connectedToRoom', { is_host: socket.isHost, room: pokerRooms[room], message: 'Welcome ' + socket.username + ', You have connected to room ' + pokerRooms[room].name });
         });
 
     });
@@ -76,26 +89,44 @@ io.on('connection', function (socket) {
         roomList();
     });
 
+    //change name of task
+    socket.on('changeTaskName', function (data) {
+        pokerRooms[data.room].task = data.name;
+        updateTaskName(data.room);
+    });
+
     //when user disconnected (might update number users)
     socket.on('disconnect', function () {
-        //console.log('user disconnected id: ' + socket.id);
-        io.to(socket.room).emit('chatMessage', socket.id + ' left the room');
+        if (pokerRooms.hasOwnProperty(socket.room)) {
+            //TODO delete room fom json if zero users.
+            delete pokerRooms[socket.room].users[socket.id];
+            chatMessage(socket.room, socket.username + ' left the room');
+            updateUsers(socket.room);
+        }
     });
 
     //planning poker functions
     socket.on('pokerPlayed', function (data) {
+        if (pokerRooms[data.room].played) {
+            chatMessage(data.room, socket.username + " has started a new round for " + pokerRooms[data.room].task);
+        }
+
         let amountPlayed = pokerRooms[data.room].played + 1;
         pokerRooms[data.room].played = amountPlayed;
         pokerRooms[data.room].numbers.push(data.value);
-        io.to(data.room).emit('chatMessage', data.user + " played " + data.value);
-        //console.log(pokerRooms);
-        if (pokerRooms[data.room].played >= pokerRooms[data.room].users) {
+        chatMessage(data.room, socket.username + " played " + data.value);
+
+        console.log(pokerRooms);
+        console.log(pokerRooms[data.room].users[socket.id]);
+        console.log(Object.keys(pokerRooms[data.room].users).length);
+
+        if (pokerRooms[data.room].played >= Object.keys(pokerRooms[data.room].users).length) {
             let total = 0;
             for (let i = 0; i < pokerRooms[data.room].numbers.length; i++) {
                 total += pokerRooms[data.room].numbers[i];
             }
             let avg = total / pokerRooms[data.room].numbers.length;
-            io.to(data.room).emit('chatMessage', "Everyone played " + avg);
+            chatMessage(data.room, "Everyone played! The result: " + avg);
             io.to(data.room).emit('pokerReset');
             pokerReset(data.room);
         }
@@ -103,24 +134,51 @@ io.on('connection', function (socket) {
 
     //chat message function (may remove)
     socket.on('chatMessage', function (data) {
-        io.to(data.room).emit('chatMessage', data.message);
+        chatMessage(data.room, data.message);
     });
 
-    function roomList(userUpdate = true) {
-        let rooms = io.sockets.adapter.rooms;
-        if(userUpdate) {
-            socket.emit('updateRoomList', rooms);
+    //global socket functions
+    function chatMessage(room, message, ignoreUser = false) {
+        if (ignoreUser) {
+            socket.broadcast.to(room).emit('newChatMessage', message);
         } else {
-            io.emit('updateRoomList', rooms);
+            io.to(room).emit('newChatMessage', message);
         }
+    }
+
+    function roomList(userUpdate = true) {
+        console.log(pokerRooms);
+        if(userUpdate) {
+            socket.emit('updateRoomList', pokerRooms);
+        } else {
+            io.emit('updateRoomList', pokerRooms);
+        }
+    }
+
+    function updateTaskName(room) {
+        chatMessage(room, "<strong>" + socket.username + "</strong> update the task name to <strong>" + pokerRooms[room].task + "</strong>" );
+        io.to(room).emit('updateTaskName', pokerRooms[room].task);
+    }
+
+    function updateUsers(room) {
+        io.to(room).emit('updateUsers', pokerRooms[room].users);
     }
 
 });
 
+//global functions
 function pokerReset(room) {
     pokerRooms[room].played = 0;
     pokerRooms[room].numbers = [];
     //console.log(pokerRooms);
+}
+
+function roomName(roomName) {
+    return  randomNames.roomnames[Math.floor(Math.random() * randomNames.roomnames.length)].name;
+}
+
+function userName(userName) {
+    return  randomNames.usernames[Math.floor(Math.random() * randomNames.usernames.length)].name;
 }
 
 http.listen(port);
---
+++ config/randomnames.js
@@ -0,0 +1,228 @@
+module.exports = {
+    usernames: [
+        {
+            "name": "Mario"
+        },
+        {
+            "name": "Samus"
+        },
+        {
+            "name": "Kirby"
+        },
+        {
+            "name": "Bowser"
+        },
+        {
+            "name": "Link"
+        },
+        {
+            "name": "Donkey Kong"
+        },
+        {
+            "name": "Fox"
+        },
+        {
+            "name": "Falco Fox"
+        },
+        {
+            "name": "Marth"
+        },
+        {
+            "name": "Zelda"
+        },
+        {
+            "name": "Sheik"
+        },
+        {
+            "name": "Villager"
+        },
+        {
+            "name": "Meta Knight"
+        },
+        {
+            "name": "Mewtwo"
+        },
+        {
+            "name": "Sonic"
+        },
+        {
+            "name": "Peach"
+        },
+        {
+            "name": "Pikachu"
+        },
+        {
+            "name": "Ice Climbers"
+        },
+        {
+            "name": "Inkling"
+        },
+        {
+            "name": "Captain Falcon"
+        },
+        {
+            "name": "Zero Suit Samus"
+        },
+        {
+            "name": "Wii Fit Trainer"
+        },
+        {
+            "name": "Squirtle"
+        },
+        {
+            "name": "Ivysaur"
+        },
+        {
+            "name": "Charizard"
+        },
+        {
+            "name": "Ness"
+        },
+        {
+            "name": "Lucas"
+        },
+        {
+            "name": "Ryu"
+        },
+        {
+            "name": "Ganondorf"
+        },
+        {
+            "name": "Ike"
+        },
+        {
+            "name": "Cloud"
+        },
+        {
+            "name": "SNAKE"
+        },
+        {
+            "name": "Jigglypuff"
+        },
+        {
+            "name": "Pichu"
+        },
+        {
+            "name": "Roy"
+        },
+        {
+            "name": "Olimar"
+        },
+        {
+            "name": "Diddy Kong"
+        },
+        {
+            "name": "Lucario"
+        },
+        {
+            "name": "Lucina"
+        },
+        {
+            "name": "Robin"
+        },
+        {
+            "name": "Bayonetta"
+        },
+        {
+            "name": "Mr Game and Watch"
+        },
+        {
+            "name": "Greninja"
+        },
+        {
+            "name": "Dr. Mario"
+        },
+        {
+            "name": "ROB"
+        },
+        {
+            "name": "Duck Hunt"
+        },
+        {
+            "name": "Pit"
+        },
+        {
+            "name": "Dark Pit"
+        },
+        {
+            "name": "Palutena"
+        },
+        {
+            "name": "Corrin"
+        },
+        {
+            "name": "Bowser Jr."
+        },
+        {
+            "name": "Toon Link"
+        },
+        {
+            "name": "Young Link"
+        },
+        {
+            "name": "King Dedede"
+        },
+        {
+            "name": "Rosalina and Luma"
+        },
+        {
+            "name": "Mii classes"
+        },
+        {
+            "name": "Wario"
+        },
+        {
+            "name": "Little Mac"
+        },
+        {
+            "name": "Pac man"
+        },
+        {
+            "name": "Shulk"
+        },
+        {
+            "name": "Wolf"
+        },
+        {
+            "name": "Mega Man"
+        },
+        {
+            "name": "Luigi"
+        },
+        {
+            "name": "Yoshi"
+        }
+    ],
+    roomnames: [
+        {
+            "name": "Drifting Planet"
+        },
+        {
+            "name": "Vacant Domain"
+        },
+        {
+            "name": "Central Earth"
+        },
+        {
+            "name": "Miniature Expanse"
+        },
+        {
+            "name": "Revised Shore"
+        },
+        {
+            "name": "Lush Archipelago"
+        },
+        {
+            "name": "Shielded Forest"
+        },
+        {
+            "name": "Dawn Rift"
+        },
+        {
+            "name": "Southern Fields"
+        },
+        {
+            "name": "Belated Region"
+        }
+    ]
+};
